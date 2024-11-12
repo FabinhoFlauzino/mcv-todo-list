69e653e65d268e5fb2db005f27fa6bf2233859a1
unknown <fabioflauzino@hotmail.com>
2024-11-12 00:37:14 -0300

    Criando projeto e conexão com banco de dados e criação de migrations

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoMVC.Models;
+using System.Diagnostics;
+
+namespace ProjetoMVC.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        HomeModel home = new HomeModel();
+
+        home.Nome = "Fábio Flauzino";
+        home.Email = "fabioflauzino@hotmail.com";
+
+        return View(home);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/HomeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjetoMVC.Models;
+
+public class HomeModel
+{
+    public string Nome { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+}
